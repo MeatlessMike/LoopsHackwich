a03ef99a1e1f1f2689838d100e62a853e7ea035e
Akivah <akivahlaviolette@gmail.com>
2022-10-24 10:35:47 -0600

    completed loops hack which

--- LoopsHackwich/ViewController.swift
+++ LoopsHackwich/ViewController.swift
@@ -14,6 +14,65 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func whenDictionaryButtonPressed(_ sender: Any) {
+   
+        var contact = ["Name":"Tom","Address":"123 Fake Street","Phone":"123-1234"]
+        
+        var output = ""
+        for (key,value) in contact {
+        output += "\(key): \(value)\n"
+        }
+        textView.text = output
+    
+    }
 
+    @IBAction func whenArray1ButtonPressed(_ sender: Any) {
+        var fruit = ["apple","pear","peach","strawberry"]
+        
+        var output = ""
+        for item in fruit {
+        output += "\(item)\n"
+        }
+        textView.text = output
+        
+    }
+    
+    @IBAction func whenArray2ButtonPressed(_ sender: Any) {
+        var veggies = ["tomato","carrot","pea","celery"]
+        
+        var output = ""
+        for i in 0...3{
+        output += "\(veggies[i])\n"
+        }
+        textView.text = output
+        
+    }
+    
+    @IBAction func whenArray3ButtonPressed(_ sender: Any) {
+   
+        var drinks = ["tea","coffee","milk","soda"]
+        
+        var output = ""
+        for i in 0..<3{
+        output += "\(drinks[i])\n"
+        }
+        textView.text = output
+        
+    }
+   
+    @IBAction func whenArray4ButtonPressed(_ sender: Any) {
+    
+        var snacks = ["chips","granola","nuts","popcorn"]
+        
+        var output = ""
+        for i in 1..<snacks.count {
+        output += "\(snacks[i])\n"
+        }
+        textView.text = output
+        
+    }
+    
 }
 
